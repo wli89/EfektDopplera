b8c2ed3f3a9ec4c225846d78223fb4ac2efdbc29
pioleb <64978863+pioleb@users.noreply.github.com>
2020-06-09 22:46:57 +0200

    działająca animacja

--- EfektDopplera/src/pl/edu/pw/fizyka/pojava/PL_WL/MainPanel.java
+++ EfektDopplera/src/pl/edu/pw/fizyka/pojava/PL_WL/MainPanel.java
@@ -15,8 +15,8 @@ public class MainPanel extends JPanel implements ActionListener 	//Piotr Lebiedz
 
 	Timer tm;
 	int time = 0;
-	int xRedObject = 200, velX = 3;
-	int xBlueObject = 600, velY = 0;
+	int xRedObject = 200, velX = 0;
+	int xBlueObject = 600, velXBlue = 0;
 	int velR = 6;
 	boolean running;
 	
@@ -25,7 +25,7 @@ public class MainPanel extends JPanel implements ActionListener 	//Piotr Lebiedz
 	private int[] circleR = new int [1000];
 	int counter = 0;
 	
-	int frequency = 10;
+	int frequency = 20;
 	boolean changeFrequency = true;
 	
 	void uruchomAnimacje()
@@ -46,7 +46,7 @@ public class MainPanel extends JPanel implements ActionListener 	//Piotr Lebiedz
         			changeFrequency = false;
         		}
         		
-        		xRedObject = xRedObject+velX;     		
+        		xRedObject = xRedObject + velX;     		
         		
         		for (int i = 0; i < counter + 1; i++) 
         		{    	
@@ -55,7 +55,7 @@ public class MainPanel extends JPanel implements ActionListener 	//Piotr Lebiedz
             		circleR[i] = circleR[i] + velR;
         		}
         		
-        		xBlueObject = xBlueObject - velY;     		
+        		xBlueObject = xBlueObject + velXBlue;     		
         		
         		time = time + 1;
         		if (time % frequency == 0)
@@ -67,9 +67,10 @@ public class MainPanel extends JPanel implements ActionListener 	//Piotr Lebiedz
 
         		repaint();
         		
-        		if (xRedObject > 778)
+        		if (xRedObject > 778 || xRedObject < 6 || xBlueObject > 778 || xBlueObject < 7)
         		{
         			tm.stop();
+        			running = false;
         		}
         	}
     	});	
@@ -90,6 +91,24 @@ public class MainPanel extends JPanel implements ActionListener 	//Piotr Lebiedz
 		}
 		
 
+	}
+	
+	public void reset() {
+		Arrays.fill(circleX, 200);
+		Arrays.fill(circleY, 300);
+		Arrays.fill(circleR, 0);
+		tm = null;
+		time = 0;
+		xRedObject = 200;
+		velX = 0;
+		xBlueObject = 600;
+		velXBlue = 0;
+		velR = 6;
+		running = false;
+		counter = 0;
+		frequency = 20;
+		changeFrequency = true;
+		repaint();
 	}
 	
 	public MainPanel()
